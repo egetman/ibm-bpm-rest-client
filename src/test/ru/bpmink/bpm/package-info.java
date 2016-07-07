1d8bbf4c0e9e29cde0d15f63f2bafdf92df48051
egetman <getman.eugene@gmail.com>
2016-07-07 17:01:03 +0300

    Source package moved deeper =). Add Test source folder.

---
+++ src/test/ru/bpmink/bpm/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Bpm rest client tests assumes that you have default installation of IBM Process Center/Server © with
+ * installed default sample app {@literal Hiring Sample}.
+ */
+package ru.bpmink.bpm;
